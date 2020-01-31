8a9ef1e8f97436776dcb138a4eeb999df26d83a3
Igor <kotigo11@gmail.com>
2020-01-31 18:14:37 +0200

    changed app.js

--- asyncTest/script.js
+++ asyncTest/script.js
@@ -2,6 +2,6 @@ function changeDiv(script){
   let test_div = document.getElementById("test");
   test_div.innerHTML += ` --- ${script} was called `;
 }
-function helloAlert() {
-  alert('hello');
+function myAlert(msg) {
+  alert(msg);
 }
